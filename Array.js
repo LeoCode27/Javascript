74eca3b96df8373d52931babac338bb03af80c22
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-14 19:35:01 +0600

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,12 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="Array.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan']
+console.log(country);
