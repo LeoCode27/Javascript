d0fc7b6fafd4adbad9053c3ea38d6cb1c9c2766d
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-14 19:39:44 +0600

    Add files via upload

---
+++ Arraylength.js
@@ -0,0 +1,13 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="Arraylength.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan']
+console.log(country.length);
+//Output-3
