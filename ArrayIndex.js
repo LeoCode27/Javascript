9153eb4c134cc6e98a05a7f9566eb5dab5fe2eb9
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-14 20:53:20 +0600

    Add files via upload

---
+++ ArrayIndex.js
@@ -0,0 +1,19 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="ArrayIndex.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan']
+var firstelement=country[0];
+var secondelement=country[1];
+var thirdelement=country[2];
+var lastelement=country[country.length-1];
+console.log(firstelement);//australia
+console.log(secondelement);//canada
+console.log(thirdelement);//japan
+console.log(lastelement);//japan
