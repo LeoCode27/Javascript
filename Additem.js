fe34f51e0cd9204aef038979e41d3267a5656c4e
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-14 21:36:58 +0600

    Add files via upload

---
+++ Additem.js
@@ -0,0 +1,19 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="ArrayIndex.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan','USA','NZD'];
+var add= country.push('UK');
+console.log(country);
+/*
+print all elemnts in country
+UK is the last element
+*/
+console.log(add);
+//print the total number of elements
