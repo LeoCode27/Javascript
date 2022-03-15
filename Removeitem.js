27cb1aae8c30f9fd3c9c2d049214408ffe179377
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-15 22:33:05 +0600

    Add files via upload

---
+++ Removeitem.js
@@ -0,0 +1,19 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="ArrayIndex.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan','USA','NZD'];
+var remove= country.pop('NZD');
+console.log(country);
+/*
+remove the last element in country
+NZD is the last element
+*/
+console.log(remove);
+//print only the removed element
