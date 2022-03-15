78c7334848aba24c09efa80050ca21c99efa62eb
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-16 00:16:14 +0600

    Add files via upload

---
+++ Additematfirst.js
@@ -0,0 +1,16 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="Additematfirst.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan','USA','NZD'];
+console.log(country);
+console.log(country.unshift('UK'))
+/*print the country['australia','canada','japan','USA','NZD'] array
+but array is length is
+*/
