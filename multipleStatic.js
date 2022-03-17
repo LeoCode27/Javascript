a41254226f56f1e98aa4822bf56f59a7b0416612
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-17 23:15:58 +0600

    Add files via upload

---
+++ multipleStatic.js
@@ -0,0 +1,16 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="mltipleStatic.js"></script>
+</head>
+</html>
+-----------------
+*/
+class Code{
+	static var1=console.log('First Static')
+	static{
+		console.log('Second Static')
+	}
+	static var2=console.log('third static')}
