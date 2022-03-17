cd61c5c299c126cf8207edc29b5ec867461672a9
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-17 15:40:02 +0600

    Add files via upload

---
+++ SpreadSyntax.js
@@ -0,0 +1,14 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="SpreadSyntax.js"></script>
+</head>
+</html>
+-----------------
+*/
+var country=['australia','canada','japan','USA','NZD'];
+console.log(country);
+console.log(shallowCopySpread=[...country])
+//[...]=SpreadSyntax
