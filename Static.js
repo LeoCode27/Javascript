a51ea6b86d513e9d9bb80500e2ace4e8f43941da
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-17 22:34:28 +0600

    Add files via upload

---
+++ Static.js
@@ -0,0 +1,19 @@
+/*
+----HTMl file----
+<html>
+<head>
+<title>LeoCode27-JS</title>
+<script src="Static.js"></script>
+</head>
+</html>
+-----------------
+*/
+class Code{
+	static var1="Javascript"
+	static var2;
+	static{
+		this.var2="Java"
+	}
+}
+console.log(Code.var1)
+console.log(Code.var2)
